d4236148f12f05ed0ca0133c193b54b0ff36067a
Luc-Govaarts <lucgovaarts@hotmail.com>
2020-07-14 15:30:27 +0200

    user model added

--- routers/auth.js
+++ routers/auth.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcrypt");
 const { Router } = require("express");
 const { toJWT } = require("../auth/jwt");
 const authMiddleware = require("../auth/middleware");
-const User = require("../models/").user;
+const User = require("../models").user
 const { SALT_ROUNDS } = require("../config/constants");
 
 const router = new Router();
@@ -36,7 +36,7 @@ router.post("/login", async (req, res, next) => {
 
 router.post("/signup", async (req, res) => {
     const { email, password, name } = req.body;
-  
+    console.log("REQUEST BODY:", req.body)
     if (!email || !password || !name) {
     return res.
       status(400).
